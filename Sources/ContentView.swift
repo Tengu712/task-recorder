8634887902d9169717d57c29cfdfb916d729c8da
Tengu712 <genreihoutengu@gmail.com>
2024-08-29 04:54:23 +0900

    feat: add textfield list

--- Sources/ContentView.swift
+++ Sources/ContentView.swift
@@ -1,7 +1,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var taskViews: [TaskView] = []
+    @State var lastId: Int = 0
+
     var body: some View {
-        Text("Hello, SwiftUI!").padding()
+        VStack {
+            // header
+            HStack {
+                Text("Tasks").font(.largeTitle)
+                Spacer()
+                Button(action: {
+                    self.taskViews.append(TaskView(id: lastId))
+                    self.lastId += 1
+                }) {
+                    Image(systemName: "plus")
+                }
+                .buttonStyle(PlainButtonStyle())
+            }
+            .padding()
+
+            // list
+            List(self.taskViews) { task in
+                HStack {
+                    task
+                    Button(action: {
+                        self.taskViews.removeAll(where: {$0.id == task.id})
+                    }) {
+                        Image(systemName: "trash")
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                }
+                .padding()
+            }
+        }
+        .background(Color.white)
     }
 }
